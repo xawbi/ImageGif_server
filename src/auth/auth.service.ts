67c61e32697629a4e478c176ae64bc98d8d49c88
xawbi <gushin.adam@yandex.com>
2024-05-03 04:44:14 +0500

    fix smtp 2

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -44,7 +44,8 @@ export class AuthService {
       service: 'gmail',
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: true,
+      secure: false,
+      requireTLS: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
